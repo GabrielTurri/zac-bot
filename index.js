e39a5d471dbe37d67d40986b07fda1024c2a5eaf
Gabriel Turri <ga-briel.t.a@hotmail.com>
2020-09-18 16:11:17 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Let\'s Bounce!');
+});
+
+// token
+client.login('your-token-here');
+
+// test command
+client.on('message', message => {
+	if (message.content === '!z ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+});
+

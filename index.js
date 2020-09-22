a144a07cf1bf92ae6f7241c5c384506b9598d632
Gabriel Turri <ga-briel.t.a@hotmail.com>
2020-09-22 15:14:08 -0300

    added commando framework

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-// require the discord.js module
-const Discord = require('discord.js');
-// create a new Discord client
-const client = new Discord.Client();
+// require the discord.js-Commando module
+const Commando = require('discord.js-commando');
+const path = require('path');
+// const sqlite = require('sqlite');
+// create a new Discord and Commando client
+const client = new Commando.Client({
+	owner: '270249938003296258',
+});
+
+client.registry
+// Registers your custom command groups
+	.registerGroups([
+		['fun', 'eae'],
+		['help', '!z help'],
+	])
+// Registers all buit-in groups, commands, and arguments types
+	.registerDefaults()
+
+// Registers all of your commands in the ./commands/ directory
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+
+// client.setProvider(
+// 	sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new Commando.SQLiteProvider(db)),
+// ).catch(console.error);
+
 
 // when the client is ready, run this
 // this event will only trigger one time after logging in
@@ -10,7 +31,7 @@ client.once('ready', () => {
 });
 
 // token
-client.login('your-token-here');
+client.login('token');
 
 // test command
 client.on('message', message => {
@@ -19,8 +40,13 @@ client.on('message', message => {
 		message.channel.send('Pong.');
 	}
 	if (message.content === 'eae' | 'Eae') {
-		// send back "Pong." to the channel the message was sent in
+		// send back "Eae beleza?" to the channel the message was sent in
 		message.channel.send('Eae beleza?');
 	}
+	if (message.content === '!z help') {
+		// send back the available commands to the channel the message was sent in
+		message.channel.send('Comandos disponíveis: ```\neae \n!z help```');
+	}
+
 });
 
